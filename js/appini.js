6b1677a677f20d3eb98e05f02f0fdffaabae3c3a
Rudi Feijo <rudibr@gmail.com>
2015-04-23 14:02:05 -0300

    dkkk
    djkk

--- js/appini.js
+++ js/appini.js
@@ -1,13 +1,19 @@
+//noconflict para caso quisermos atualizar o jquery do app no futuro
+var $1 = jQuery.noConflict();
+
 ﻿CP = {online: true};
 CP.jsv = Math.ceil(Math.random() * 999999999999999) + 1;
+CP.APP_NAME = 'multidados_exemplos';
 
 //cookie para debug
 if (document.cookie.toString().indexOf('RDD=RDD') === -1) {
-    CP.URL_API = 'http://200.155.13.171:8080/m_apps/multidados_exemplosw/';
-    CP.URL_APP = 'http://200.155.13.171:8080/m_apps/multidados_exemplosw/';
+    CP.URL_API = 'http://h1.multidadosti.com.br:8080/m_apps/multidados_exemplosw/';
+    CP.URL_APP = 'http://h1.multidadosti.com.br:8080/m_apps/multidados_exemplosw/';
+    CP.URL_PUB = 'http://h1.multidadosti.com.br:8080/m_apps/public/';
 } else {
     CP.URL_APP = 'http://127.0.0.1/m_apps/multidados_exemplosw/';
     CP.URL_API = 'http://127.0.0.1/m_apps/multidados_exemplosw/';
+    CP.URL_PUB = 'http://127.0.0.1/m_apps/public/';
 }
 
 MSG_SEM_NET = "Sua conexão com a internet parece estar desligada. Por favor verifique sua conexão e tente de novo.";
@@ -33,35 +39,35 @@ function app_connected() {
 }
 
 function load_ini_script() {
-    $('#divsemnet').remove();
+    $1('#divsemnet').remove();
 
-    $('body').css({display: 'none', visibility: 'hidden'});
+    $1('body').css({display: 'none', visibility: 'hidden'});
 
-    $('html').css({backgroundColor: '#FFF'
+    $1('html').css({backgroundColor: '#FFF'
         , backgroundImage: 'url("' + CP.URL_APP + 'imgs/splash_loading.png")'
         , backgroundRepeat: 'no-repeat'
         , backgroundAttachment: 'fixed'
         , backgroundPosition: 'center'});
 
-    $('head').append('<script' + ' type="text/javascript"' + ' src="' + CP.URL_APP + 'js/app.js?v=' + CP.jsv + '"' + '><' + '/' + 'script>');
+    $1('head').append('<script' + ' type="text/javascript"' + ' src="' + CP.URL_APP + 'js/app.js?v=' + CP.jsv + '"' + '><' + '/' + 'script>');
 
     window.load_ini_script_interval = window.setInterval(function() {
-        if ($('body').hasClass('ui-mobile-viewport')) {//verificao confiavel que o jqm terminou de renderizar
+        if ($1('body').hasClass('ui-mobile-viewport')) {//verificao confiavel que o jqm terminou de renderizar
 
-            $('html').css({backgroundColor: ''
+            $1('html').css({backgroundColor: ''
                 , backgroundImage: ''
                 , backgroundRepeat: ''
                 , backgroundAttachment: ''
                 , backgroundPosition: ''});
 
-            $('body').css({display: '', visibility: 'visible'});
+            $1('body').css({display: '', visibility: 'visible'});
 
             window.clearInterval(window.load_ini_script_interval);
         }
     }, 100);
 }
 
-$(function() {
+$1(function() {
     var wait_one_int = true;
     if (app_connected()) {
         load_ini_script();
@@ -74,8 +80,8 @@ $(function() {
                 if (wait_one_int) {
                     wait_one_int = false;
                 } else {
-                    $('#divsemnet').remove();
-                    $('body').append('<div id="divsemnet" style="font-family:Arial">' + MSG_SEM_NET + '</div>');
+                    $1('#divsemnet').remove();
+                    $1('body').append('<div id="divsemnet" style="font-family:Arial">' + MSG_SEM_NET + '</div>');
                 }
             }
         }, 1000);
